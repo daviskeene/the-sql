c4985c9308e71f515145f0049859d53da3bc2ad2
Davis Keene <dbkeene.tsyc@gmail.com>
2021-04-01 13:50:33 -0500

    update: hot reload api, update requirements

--- api/main.py
+++ api/main.py
@@ -4,7 +4,10 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "Hello World!"
+    return ({
+        'hello': 'world',
+        'goodbye': 'earth'
+    })
 
 if __name__ == '__main__':
-    app.run()
+    app.run(port=8000, debug=True)
